3d687df05a39b497a5dbe0abf831263bba6cb38d
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-04 13:15:26 -0400

    edited code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Zaida Hammel
+// Created on: May 2022
 // This file contains the JS functions for index.html
 
-function myButtonClicked() {
-  // this function displays "Hello, World!"
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+"use strict";
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-04-JS/sw.js", {
+    scope: "/ICS2O-Unit5-04-JS/",
+  });
 }
+
+/**
+ * This function determines what angles create what triangle.
+ */
+ function myButtonClicked() {
+   var angle1
+   var angle2
+   var angle3
+
+   if ((angle1 == angle2) && (angle2 == angle3)) {
+    document.getElementById("answers").innerHTML = "Your triangle is an equilateral triangle."
+   }
+   else if ((angle1 == angle2) || (angle2 == angle3) || (angle3 == angle1)) {
+    document.getElementById("answers").innerHTML = "Your triangle is an isosceles triangle."
+   }
+   else {
+    document.getElementById("answers").innerHTML = "Your triangle is a scalene triangle."
+   }
+ }
